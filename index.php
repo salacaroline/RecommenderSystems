e1626870e4b73a658da47cf857160fa342d4f4b5
salacaroline <carolinesala16@gmail.com>
2020-08-15 19:11:14 -0300

    Adicionado pop up

--- index.php
+++ index.php
@@ -16,6 +16,31 @@
                 }
             }
         </script>
+        <script type="text/javascript" src="js/bioep.min.js"></script>
+            <script type="text/javascript">
+              // bioEp.init({
+              //   html: '<div id="content">This is a simple popup</div>',
+              //   css: '#content {font-family: "Titillium Web", sans-serif; font-size: 14px;}',
+              //       cookieExp: 0
+              // });
+              bioEp.init({
+                html: '<div id="bio_ep_content">' +
+                    '<span></br></span>' +
+                    '<span>Não esqueça de avaliar! :)</br></br></span>' +
+                    '<span>Não esqueça de avaliar as recomendações com os botões Gostei/Não gostei e enviar a avaliação no fim da página.</br> Obrigada por participar! </span>' +
+                    '<span></span>' +
+                    '' +
+                    '</div>',
+                css: '#bio_ep {width: 400px; height: 300px; color: #333; background-color: #fafafa; text-align: center;}' +
+                    '#bio_ep_content {padding: 24px 0 0 0; font-family: "Titillium Web";}' +
+                    '#bio_ep_content span:nth-child(2) {display: block; color: blue; font-size: 32px; font-weight: 600;}' +
+                    '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
+                    '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
+                    '.bio_btn {display: inline-block; margin: 18px 0 0 0; padding: 7px; color: #fff; font-size: 14px; font-weight: 600; background-color: #70bb39; border: 1px solid #47ad0b; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 0; text-decoration: none;}',
+                fonts: ['//fonts.googleapis.com/css?family=Titillium+Web:300,400,600'],
+                cookieExp: 0
+              });
+            </script>
     </head>
     <body>
         
