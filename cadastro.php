2c821421ffea8f467752def09212ab0ea2c20c88
salacaroline <carolinesala16@gmail.com>
2020-08-15 19:24:50 -0300

    ajuste nos checks de cadastro

--- cadastro.php
+++ cadastro.php
@@ -30,7 +30,7 @@ for ($i = 1;$i < 11;$i++)
 <head>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
-  <link rel="stylesheet" type="text/css" href="css/css.css">
+  <link rel="stylesheet" type="text/css" href="css/css3.css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
